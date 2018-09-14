9f03177876b37e46dccbf3549adc696ca8c95b05
John Allen <jallen4444@gmail.com>
2018-09-14 02:17:20 -0400

    The database is now functional and post is working to add records to the database. Reorganized some code in the main app.

--- app/people.py
+++ app/people.py
@@ -1,9 +1,7 @@
 #app/__init__.py
 import os
-from flask import Flask
+from flask import Flask, request, flash, url_for, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
-from flask import render_template
-from flask import request
 
 #Find relative path to the DB
 project_dir = os.path.dirname(os.path.abspath(__file__))
@@ -11,26 +9,11 @@ database_file = "sqlite:///{}".format(os.path.join(project_dir, "people.db"))
 
 #Initialize app
 app = Flask(__name__)
-
-#Load views
-#from app import models
-
-#Load config
 app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+app.config['SECRET_KEY'] = "random string"
 
 #Initializing DB
 db = SQLAlchemy(app)
-
-#Main Page
-@app.route('/', methods=["GET", "POST"])
-def index():
-    if request.form:
-        people = People(name=request.form.get(["name","phone", "city", "state"]))
-        db.session.add(people)
-        db.session.commit()
-        #print(request.form)
-    return render_template("index.html")
-
 
 class People(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -39,5 +22,25 @@ class People(db.Model):
     city = db.Column(db.String(50), nullable=False)
     state = db.Column(db.String(50), nullable=False)
 
+    def __init__(self, name, phone, city, state):
+        self.name = name
+        self.phone = phone
+        self.city = city
+        self.state = state
+
+#Main Page
+@app.route('/', methods=["GET", "POST"])
+def index():
+    if request.method == 'POST':
+        #people = People(name=request.form.get(["name","phone", "city", "state"]))
+        people = People(request.form["name"], request.form["phone"], request.form["city"], request.form["state"])
+        db.session.add(people)
+        db.session.commit()
+        flash('Record was successfully added')
+        #print(request.form)
+    #people = People.query.all()
+    return render_template("index.html")
+
     def __repr__(self):
-        return f"People('{self.name}','{self.phone}','{self.city}','{self.state}')"
+        #return f"People('{self.name}','{self.phone}','{self.city}','{self.state}')"
+        return f"People('{self.name}')"
